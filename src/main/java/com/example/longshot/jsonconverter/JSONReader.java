3ad3489559b15f6d1dc40e2765404e73343fe789
Niharika Puvvada <lpuvvad@homeoffice.wal-mart.com>
2018-06-22 16:04:28 +0530

    resourcechanges

--- src/main/java/com/example/longshot/jsonconverter/JSONReader.java
+++ src/main/java/com/example/longshot/jsonconverter/JSONReader.java
@@ -24,11 +24,11 @@ public class JSONReader {
         SearchResult searchRes = null;
 
         if (algo.equalsIgnoreCase("algo1"))
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot/json/product1.json"), SearchResult.class);
+            searchRes = objectMapper.readValue(new File("/resources/json/product1.json"), SearchResult.class);
         else if (algo.equalsIgnoreCase("algo2"))
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot/json/product2.json"), SearchResult.class);
+            searchRes = objectMapper.readValue(new File("/resources/json/json/product2.json"), SearchResult.class);
         else
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot/json/product3.json"), SearchResult.class);
+            searchRes = objectMapper.readValue(new File("/resources/json/json/product3.json"), SearchResult.class);
         return searchRes;
     }
 
@@ -38,7 +38,7 @@ public class JSONReader {
         AddQueryReponse searchRes = null;
 
         if (searchTerm != null)
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot/json/searchTermResponse.json"),
+            searchRes = objectMapper.readValue(new File("/resources/json/searchTermResponse.json"),
                     AddQueryReponse.class);
         return searchRes;
     }
@@ -51,60 +51,60 @@ public class JSONReader {
         //Default (No re-Reank)
         if (query.getResponsetype() == null || query.getResponsetype().equals("")) {
             if (query.getKeyword().equalsIgnoreCase("milk"))
-                searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/milk_Default.json"),
+                searchRes = objectMapper.readValue(new File("/resources/json/milk_Default.json"),
                         SearchServiceDTO.class);
             else if (query.getKeyword().equalsIgnoreCase("bread"))
-                searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/bread_Default.json"),
+                searchRes = objectMapper.readValue(new File("/resources/json/bread_Default.json"),
                         SearchServiceDTO.class);
             else if (query.getKeyword().equalsIgnoreCase("tomoto"))
-                searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/tomoto_Default.json"),
+                searchRes = objectMapper.readValue(new File("/resources/json/tomoto_Default.json"),
                         SearchServiceDTO.class);
             else if (query.getKeyword().equalsIgnoreCase("banana"))
-                searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/banana_Default.json"),
+                searchRes = objectMapper.readValue(new File("/resources/json/banana_Default.json"),
                         SearchServiceDTO.class);
         }
         
         //Clickstream_Constant_Threshold
         else if (query.getKeyword().equalsIgnoreCase("milk") && query.getResponsetype().equalsIgnoreCase("algo1"))
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/milk_algo1.json"), SearchServiceDTO.class);
+            searchRes = objectMapper.readValue(new File("/resources/json/milk_algo1.json"), SearchServiceDTO.class);
         else if (query.getKeyword().equalsIgnoreCase("bread") && query.getResponsetype().equalsIgnoreCase("algo1"))
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/bread_algo1.json"),
+            searchRes = objectMapper.readValue(new File("/resources/json/bread_algo1.json"),
                     SearchServiceDTO.class);
         else if (query.getKeyword().equalsIgnoreCase("tomoto") && query.getResponsetype().equalsIgnoreCase("algo1")) {
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/tomoto_algo1.json"), SearchServiceDTO.class);
+            searchRes = objectMapper.readValue(new File("/resources/json/tomoto_algo1.json"), SearchServiceDTO.class);
         }
         else if (query.getKeyword().equalsIgnoreCase("banana") && query.getResponsetype().equalsIgnoreCase("algo1")) {
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/banana_algo1.json"), SearchServiceDTO.class);
+            searchRes = objectMapper.readValue(new File("/resources/json/banana_algo1.json"), SearchServiceDTO.class);
         }
         
       //Clickstream Relative  Threshold
         else if (query.getKeyword().equalsIgnoreCase("banana") && query.getResponsetype().equalsIgnoreCase("algo2")) {
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/banana_algo2.json"),
+            searchRes = objectMapper.readValue(new File("/resources/json/banana_algo2.json"),
                     SearchServiceDTO.class);
         }
         else if (query.getKeyword().equalsIgnoreCase("bread") && query.getResponsetype().equalsIgnoreCase("algo2")) {
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/bread_algo2.json"),
+            searchRes = objectMapper.readValue(new File("/resources/json/bread_algo2.json"),
                     SearchServiceDTO.class);
         }
         else if (query.getKeyword().equalsIgnoreCase("tomoto") && query.getResponsetype().equalsIgnoreCase("algo2")) {
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/tomoto_algo2.json"),
+            searchRes = objectMapper.readValue(new File("/resources/json/tomoto_algo2.json"),
                     SearchServiceDTO.class);
         }
         else if (query.getKeyword().equalsIgnoreCase("milk") && query.getResponsetype().equalsIgnoreCase("algo2")) {
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/milk_algo2.json"),
+            searchRes = objectMapper.readValue(new File("/resources/json/milk_algo2.json"),
                     SearchServiceDTO.class);
             
         //Merchadizing Boost Bury Rules
         }else if (query.getKeyword().equalsIgnoreCase("bread") && query.getResponsetype().equalsIgnoreCase("assembler")) {
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/bread_Boost.json"),
+            searchRes = objectMapper.readValue(new File("/resources/json/bread_Boost.json"),
                     SearchServiceDTO.class);
         } else if (query.getKeyword().equalsIgnoreCase("banana") && query.getResponsetype().equalsIgnoreCase("assembler")) {
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/banana_Boost.json"), SearchServiceDTO.class);
+            searchRes = objectMapper.readValue(new File("/resources/json/banana_Boost.json"), SearchServiceDTO.class);
         }
         else if (query.getKeyword().equalsIgnoreCase("tomoto") && query.getResponsetype().equalsIgnoreCase("assembler")) {
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/tomoto_Boost.json"), SearchServiceDTO.class);
+            searchRes = objectMapper.readValue(new File("/resources/json/tomoto_Boost.json"), SearchServiceDTO.class);
         }else if (query.getKeyword().equalsIgnoreCase("milk") && query.getResponsetype().equalsIgnoreCase("assembler")) {
-            searchRes = objectMapper.readValue(new File("C:/app/Longshot1/json/milk_Boost.json"), SearchServiceDTO.class);
+            searchRes = objectMapper.readValue(new File("/resources/json/milk_Boost.json"), SearchServiceDTO.class);
         }
         
         List<Items> itemList = searchRes.getItems();
